3dd2900885905627ad15a21f18053319b866ef39
Siji Olaiya <Sijiolaiya@gmail.com>
2023-06-15 21:42:47 -0700

    display a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char first[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(first);
+	return (0);
+}
