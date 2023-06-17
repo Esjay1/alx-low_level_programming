32f25c675dbac4628f9b498d2bcddb0515b7b5ed
Siji Olaiya <Sijiolaiya@gmail.com>
2023-06-16 17:32:44 -0700

    using putchar function with integers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tester;  /* declare the variable */
+
+	for (tester = 48; tester <= 57; tester++)
+	{
+		putchar(tester);
+	}
+	putchar('\n');
+	return (0);
+}
