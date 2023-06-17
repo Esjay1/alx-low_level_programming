a5659e91f16cf728068a31fb2f61b48d9509b5cf
Siji Olaiya <Sijiolaiya@gmail.com>
2023-06-16 17:15:00 -0700

    display digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tester;  /* declare the variables */
+
+	for (tester = 0; tester <= 9; tester++)
+	{
+		printf("%d", tester);
+	}
+	printf("\n");
+	return (0);
+}
