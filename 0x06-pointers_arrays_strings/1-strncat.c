a7d97567e1cef6ae75db8e01a6eb04fa6c0abe61
Siji Olaiya <Sijiolaiya@gmail.com>
2023-06-30 16:21:45 -0700

    concatenate a number of chard

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenate up to a number of specific times
+ * @dest: the string to be added to
+ * @src: the source string to be added to dest
+ * @n: the number of characters to be concatenated
+ *
+ * Return: the new dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len;
+	int src_len;
+
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		len = len + 1;
+	}
+	src_len = 0;
+	while (src_len < n)
+	{
+		dest[len] = src[src_len];
+		len++;
+		src_len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
