5f9505f6ed21bb49696a65dbb3f7c2fe8c07f5d4
Siji Olaiya <Sijiolaiya@gmail.com>
2023-07-13 16:42:49 -0700

    concatenate two strings with n chars from string 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = lens2;
 	}
 	sum = lens1 + n + 1;
-	pointer = malloc(sum);
+	pointer = malloc(sum * sizeof(char));
 	if (pointer == NULL)
 	{
 		return (NULL);
