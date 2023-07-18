2200634c878028700dd833ef0208ee722ac10c74
Siji Olaiya <Sijiolaiya@gmail.com>
2023-07-18 15:46:34 -0700

    dog typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - for storing a dog's data
+ * @name: pointer to the dog's name
+ * @age: it's age
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ *  * dog_t - typedef for struct dog
+ *   */
+typedef struct dog dawg;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dawg *new_dog(char *name, float age, char *owner);
+
+#endif
+
