7eadfb0601fdaae5d71bd7e017c2a2ccbd8c4a9e
Sijibomi Olaiya <sijiolaiya@gmail.com>
2023-12-06 00:31:48 -0800

    function that prints a square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,23 +10,25 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
-	if (n > 0)
+	if(n <= 0)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (i = 0; i <= n; i++)
-			{	
-				_putchar('\\');
-				_putchar('$');
-				_putchar('\n');
-				_putchar(' ');
-			}
-			_putchar(' ');
-		}
-	}
+		_putchar('$');
+		_putchar('\n');
+	}	
 	else
 	{
-		_putchar('\n');
+		for (i = 0; i == n; i++)
+		{
+			for (j = 0; j == n; j++)
+			{
+				_putchar(' ');
+			}
+			j = 0;
+			_putchar('$');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
